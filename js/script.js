4a66f88228b639d691ff8fa0939ce0ecc5e11f8e
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-07 17:15:07 +0200

    [UPD] pari-o-dispari-new

--- js/script.js
+++ js/script.js
@@ -19,46 +19,59 @@
 // //stampo il risultato
 // console.log(reversedWord(word));
 
-//chiedo all'utente di inserire una parola
-const word = prompt("Inserire una parola");
-console.log(word);
+// //chiedo all'utente di inserire una parola
+// const word = prompt("Inserire una parola");
+// console.log(word);
 
-function reversedWord(text) {
-  //definizione variabile
-  let palindromeWord = ``;
+// function reversedWord(text) {
+//   //definizione variabile
+//   let palindromeWord = ``;
 
-  //ciclo per invertire parola
-  for (let i = text.length - 1; i >= 0; i--) {
-    palindromeWord += text[i];
-  }
+//   //ciclo per invertire parola
+//   for (let i = text.length - 1; i >= 0; i--) {
+//     palindromeWord += text[i];
+//   }
 
-  //verifico se la parola è palindroma
-  if (text === palindromeWord) {
-    return `La parola "${text}" è palindroma`;
-  } else {
-    return `La parola "${text}" NON è palindroma`;
-  }
-}
+//   //verifico se la parola è palindroma
+//   if (text === palindromeWord) {
+//     return `La parola "${text}" è palindroma`;
+//   } else {
+//     return `La parola "${text}" NON è palindroma`;
+//   }
+// }
 
-//stampo il risultato
-console.log(reversedWord(word));
+// //stampo il risultato
+// console.log(reversedWord(word));
 
 //PARI E DISPARI
 //inserisco o pari o dispari
 let even = `pari`;
 let odd = `dispari`;
 
-let choice = prompt (`Scegli tra pari e dispari`)
+let userChoice = prompt(`Scegli tra pari e dispari`);
+
+let result;
+
+if (userChoice === `pari`) {
+  result = even;
+} else if (userChoice === `dispari`) {
+  result = odd;
+} else {
+  result = `Scelta non valida`;
+}
+
+console.log(`Hai scelto ` + result);
+
 //inserire un numero tra 1 e 5
+
 //generare un numero random tramite funzione compreso tra 1 e 5
 //faccio la somma dei due numeri
 //controllo se la somma è pari o dispari
-//se ho scritto pari ed è pari ho vinto 
-//se ho scelto dispari ed è dispari ho vinto 
-//se invece ho inserito pari e la somma è dispari ho perso 
+//se ho scritto pari ed è pari ho vinto
+//se ho scelto dispari ed è dispari ho vinto
+//se invece ho inserito pari e la somma è dispari ho perso
 //se la ho inserito dispari e la somma è pari ho perso
 
-
-//fare due funzioni: 
+//fare due funzioni:
 //funzione 1: funzione che genera il numero random
 //funzione 2: funzione che stabilisce se la somma dei due numeri è pari o dispari
