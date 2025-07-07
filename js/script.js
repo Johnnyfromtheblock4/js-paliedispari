726aef7f5a2a5940d9d7725a89d30f2ed8fc5bef
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-07 16:41:58 +0200

    [UPD] for

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
-//Palindroma
+// //Palindroma
+
+// //chiedo all'utente di inserire una parola
+// const word = prompt("Inserire una parola");
+// console.log(word);
+
+// function reversedWord(text) {
+//   //converto la stringa in un array
+//   const palindromeWord = text.split("").reverse().join("");
+
+//   //verifico se la parola è palindroma
+//   if (text === palindromeWord) {
+//     return `La parola "${text}" è palindroma`;
+//   } else {
+//     return `La parola "${text}" NON è palindroma`;
+//   }
+// }
+
+// //stampo il risultato
+// console.log(reversedWord(word));
 
 //chiedo all'utente di inserire una parola
 const word = prompt("Inserire una parola");
 console.log(word);
 
 function reversedWord(text) {
-  //converto la stringa in un array
-  const palindromeWord = text.split("").reverse().join("");
+  //definizione variabile
+  let palindromeWord = ``;
+
+  //ciclo per invertire parola
+  for (let i = text.length - 1; i >= 0; i--) {
+    palindromeWord += text[i];
+  }
 
   //verifico se la parola è palindroma
   if (text === palindromeWord) {
@@ -16,5 +40,4 @@ function reversedWord(text) {
   }
 }
 
-//stampo il risultato
 console.log(reversedWord(word));
