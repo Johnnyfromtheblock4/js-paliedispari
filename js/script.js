0b01bce45ab2de284c91810289fa77c03411c256
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-07 16:19:03 +0200

    [UPD] join

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 //Palindroma
 
 //chiedo all'utente di inserire una parola
-const word = prompt(`Inserire una parola`);
+const word = prompt("Inserire una parola");
 console.log(word);
 
-function reverseWord(text) {
+function reversedWord(text) {
   //converto la stringa in un array
-  palindromeWord = text.split("").reverse();
+  const palindromeWord = text.split("").reverse().join("");
 
   //verifico se la parola è palindroma
   if (text === palindromeWord) {
-    console.log(`La parola ${text} è palindroma`);
+    return `La parola "${text}" è palindroma`;
   } else {
-    console.log(`La parola ${text} NON è palindroma`);
+    return `La parola "${text}" NON è palindroma`;
   }
 }
 
 //stampo il risultato
-console.log(reverse.Word(word));
-
+console.log(reversedWord(word));
