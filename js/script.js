c8999083f5437de8ecff79880eab45e246da8944
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-07 16:00:47 +0200

    [UPD] string-to-array

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@
 //chiedo all'utente di inserire una parola
 const word = prompt(`Inserire una parola`);
 console.log(word);
+
+function reversedWord(text) {
+  //converto la stringa in un array
+  palindromeWord = text.split("").reverse();
+  console.log(palindromeWord);
+}
